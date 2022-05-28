25a34a002eb4eb02c78443ff98f8606e58856120
marie_l <mariemlaqua@gmail.com>
2022-05-28 10:07:18 -0700

    Inital commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//Select Your Elements
+var subTypeElement = document.querySelector("#subscription");
+var subDurationElement = document.querySelector("#months");
+var result = document.querySelector(".result");
+var subType = "basic";
+var subDuration = Number("1");
+
+//Create Change Event Listeners
+subTypeElement.addEventListener("change", function (e) {
+	subType = e.target.value;
+	// console.log(subType);
+	updateSubscriptionDiv();
+});
+
+subDurationElement.addEventListener("change", function (e) {
+	subDuration = Number(e.target.value);
+	// console.log(subDuration);\
+	updateSubscriptionDiv();
+});
+
+//Use a Function to Calculate the Cost
+var updateSubscriptionDiv = function () {
+	var monthlyCost = Number("5");
+	if (subType === "standard") {
+		monthlyCost = Number("7");
+	} else if (subType === "premium") {
+		monthlyCost = Number("10");
+	}
+	var total = subDuration * monthlyCost;
+	result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`;
+};
